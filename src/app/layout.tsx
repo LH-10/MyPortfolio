9698bcc8159d467fc60e8b3c9680de9a870a1c9d
LalitH <155745277+LH-10@users.noreply.github.com>
2025-09-19 18:50:28 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Lalit Hinduja Portfolio",
   description: "Hey I'm Lalit Hinduja  a full-stack web developer , I'm currently expanding into desktop app devlopment . Explore projects, resume in my portfolio.",
-  keywords:"lalit hinduja,Lalit,Hinduja,portfolio,LH-10,web developer, frontend developer, fullstack,lalit portfolio ",
+  keywords:"lalit hinduja,Lalit,Hinduja,portfolio,LH-10,web developer, frontend developer, fullstack,lalit portfolio ,Golang dev, GO",
   authors:{name:"Lalit Hinduja"}
 };
 
@@ -28,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="U_wDVMKDR2TUAlR1csZLXj9COx-vHA8UtG7nr_m55KY" />
+        <meta name="msvalidate.01" content="DCA6585C76E074EE39921041663092EB" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
